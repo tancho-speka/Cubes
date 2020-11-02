90468bc52086d67b0929d9cdf713394a36e2a3c1
Ta-ty-na-Speka <spekatancho@gmail.com>
2020-11-02 18:27:04 +0200

    Sourse code

---
+++ script.js
@@ -0,0 +1,97 @@
+"use strict";
+
+// Функция добавления и удаления строки и столбца
+function addRow() {
+  let element = document.querySelector("tbody");
+  let tr = document.createElement("tr");
+  tr.className = "trow";
+  element.append(tr);
+
+  let count = document.querySelector("tr");
+
+  for (let i = 0; i < count.childElementCount; i++) {
+    let td = document.createElement("td");
+    tr.append(td);
+  }
+}
+
+function addColumn() {
+  let columns = document.querySelectorAll(".trow");
+  columns.forEach((element) => {
+    let td = document.createElement("td");
+    element.append(td);
+  });
+}
+
+// Прикрепление функции добавления и удаления строки к кнопкам
+
+let rightButton = document.querySelector(".button-right");
+rightButton.addEventListener("click", addColumn);
+
+let downButton = document.querySelector(".button-down");
+downButton.addEventListener("click", addRow);
+
+// Функции удаления столбца и строки
+
+let rowIndex = 0;
+let cellIndex = 0;
+
+function removeRow() {
+  let row = document.querySelectorAll(".trow");
+  row[rowIndex].remove();
+  hideButtons();
+}
+
+function removeColumn() {
+  let rows = document.querySelectorAll(".trow");
+  rows.forEach((element) => {
+    element.children[cellIndex].remove();
+  });
+  hideButtons();
+}
+
+//  Прикрепление функции удаления столбца и строки к кнопкам
+
+let rightButtonMinus = document.querySelector(".button-right-minus");
+rightButtonMinus.addEventListener("click", removeColumn);
+
+let downButtonMinus = document.querySelector(".button-down-minus");
+downButtonMinus.addEventListener("click", removeRow);
+
+let table = document.querySelector("table");
+
+table.addEventListener("mouseleave", hideButtons);
+table.addEventListener("mouseenter", showButtons);
+
+let moveBlock = document.getElementById("button-minus1");
+let moveBlock2 = document.getElementById("button-minus2");
+
+function showButtons() {
+  let rows = document.querySelectorAll(".trow");
+  if (rows.length > 1) {
+    document.getElementById("button-minus2").style.display = "block";
+  }
+  if (rows[0].childElementCount > 1) {
+    document.getElementById("button-minus1").style.display = "block";
+  }
+}
+
+function hideButtons() {
+  document.getElementById("button-minus1").style.display = "none";
+  document.getElementById("button-minus2").style.display = "none";
+}
+
+moveBlock.addEventListener("mouseenter", showButtons);
+moveBlock2.addEventListener("mouseenter", showButtons);
+
+moveBlock.addEventListener("mouseleave", hideButtons);
+moveBlock2.addEventListener("mouseleave", hideButtons);
+
+document.addEventListener("mouseover", function (event) {
+  if (event.target.nodeName === "TD") {
+    cellIndex = event.target.cellIndex;
+    rowIndex = event.target.parentNode.rowIndex;
+    moveBlock.style.transform = "translateX(" + event.target.offsetLeft + "px)";
+    moveBlock2.style.transform = "translateY(" + event.target.offsetTop + "px)";
+  }
+});
